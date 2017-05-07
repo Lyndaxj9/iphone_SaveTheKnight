75e5a6a0f9b2053f44ce516bd0fbe41d97c45a8c
Lynda <Lyndaxj9@users.noreply.github.com>
2017-05-06 22:23:36 -0400

    added tileMap for ground. Physicsbody for ground and started on player class (SKSpriteNode).

--- p08ademola/p08ademola/GameScene.swift
+++ p08ademola/p08ademola/GameScene.swift
@@ -10,8 +10,25 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var groundMap:SKTileMapNode!
+    
+    let groundNode = SKNode()
+    
     override func didMove(to view: SKView) {
         print("GameScene()")
+        groundNode.position = CGPoint(x: -669, y: -280)
+        groundNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 1334, height: 190) , center: CGPoint(x:1334/2, y:0))
+        groundNode.physicsBody?.affectedByGravity = false
+        groundNode.physicsBody?.allowsRotation = false
+        addChild(groundNode)
+        
+        guard let groundMap = childNode(withName: "GroundMap") as? SKTileMapNode else {
+            fatalError("Background node not loaded")
+        }
+        self.groundMap = groundMap
     }
     
+    override func update(_ currentTime: TimeInterval) {
+        groundMap.position = CGPoint(x: groundMap.position.x - 5, y: groundMap.position.y)
+    }
 }
---
+++ p08ademola/p08ademola/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  p08ademola
+//
+//  Created by Lynda on 5/6/17.
+//  Copyright © 2017 Lynda. All rights reserved.
+//
+
+import SpriteKit
+
+class Player: SKSpriteNode {
+
+}
